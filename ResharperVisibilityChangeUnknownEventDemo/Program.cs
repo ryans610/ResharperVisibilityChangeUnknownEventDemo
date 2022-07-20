6cf47d0e2b7d9c640c183913221b81a31ffbe91c
ryans610 <ryans610@gmail.com>
2022-07-20 17:05:08 +0800

    init

---
+++ ResharperVisibilityChangeUnknownEventDemo/Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.Run();
---
+++ ResharperVisibilityChangeUnknownEventDemo/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿document.addEventListener("visibilitychange", function () {
+    if (document.visibilityState === "visible") {
+        document.getElementById("display").value += `${new Date()} visible\n`;
+    } else {
+        document.getElementById("display").value += `${new Date()} not visible\n`;
+    }
+});
